a131497a3bd2e522fcd8cd7d37cde2516619a1b6
Carmelo C <carmelo.califano@gmail.com>
2020-04-03 12:58:52 +0200

    2020-04-03: refactored to run under Python 3 interpreter

--- ssl_test_ciphers.py
+++ ssl_test_ciphers.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Scans a remote host to display all available common TLS ciphers
 # author: carmelo.califano@gmail.com
 # history, date format ISO 8601:
+#  2020-04-03: 2.0 moved to Python3, polished a few quirks
 #  2016-10-14: 1.0 initial version
 
 # Import some modules
-from __future__ import print_function	# print() as a function not as a statement
 import argparse				# write user-friendly command-line interfaces
 import os				# use operating system dependent functionalities
 import subprocess			# spawn new processes, connect to their input/output/error pipes, and obtain their return codes
 import sys				# variables used or maintained by the interpreter and functions that interact strongly with the interpreter
 
 # Version number
-__version__ = 1.0
-__build__ = 161015
+__version__ = "2.0"
+__build__ = "20200403"
+EPILOG=' '
 
 # https://svn.blender.org/svnroot/bf-blender/trunk/blender/build_files/scons/tools/
 class bcolors:
@@ -42,10 +43,10 @@ def validIP(ipAddrs):
   return True
 
 def main():
-  parser = argparse.ArgumentParser(description='Scans <IP address>:<IP address> querying all the supported TLS ciphers, version {version}, build {build}.'.format(version=__version__, build=__build__))
+  parser = argparse.ArgumentParser(description='Scans <IP address>:<IP address> querying all the supported TLS ciphers, version ' + __version__ + ', build ' + __build__ + '.', epilog=EPILOG)
   parser.add_argument('ipAdd', metavar='<IP address>', type=str, help='IP address of the host to scan for ciphers')
   parser.add_argument('whichPort', metavar='<Port number>', type=str, help='Port number, usually equals 443')
-  parser.add_argument('-V', '--version', action='version', version='%(prog)s {version}'.format(version=__version__))
+  parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__ + '')
 
   # In case of no arguments print help message then exits
   if len(sys.argv) == 1:
@@ -56,32 +57,37 @@ def main():
 
   # Let's validate input
   if not validIP(args.ipAdd):
-    print('Address is not a valid IPv4 one!')
-    print('Exiting...', end='\n\n')
+    print('[-] Address is not a valid IPv4 one!')
+    print('[-] Exiting...', end='\n\n')
     sys.exit(2)
 
   if (int(args.whichPort) < 1) or (int(args.whichPort) > 65535):
-    print('Port out of range!')
-    print('Exiting...', end='\n\n')
+    print('[-] Port out of range!')
+    print('[-] Exiting...', end='\n\n')
     sys.exit(3)
 
   # If input is OK we can continue. First we fetch the output of openssl ciphers run on the local machine
   # this will give us a string with all the locally supported ciphers
   sslVer = subprocess.check_output(['openssl', 'version'])
   print('[+] ' + os.uname()[0] + ' version ' + os.uname()[2] + ' running on ' + os.uname()[4] + ' platform')
-  print('[+] ' + sslVer.rstrip() + ', fetching the list of locally supported ciphers...')
+  print('[+] ' + sslVer.decode().rstrip() + ', fetching the list of locally supported ciphers...')
   myCiphers = subprocess.check_output(['openssl', 'ciphers', 'ALL:eNULL'])
   # Transform string into a list
-  lCiphers = myCiphers.rstrip('\n').split(':')
+  lCiphers = myCiphers.decode().rstrip('\n').split(':')
   for item in lCiphers:
     # Here we build the command string, stderr is redirected to stdout to keep output clean
     cmdstr = 'echo -n | openssl s_client -cipher ' + item + ' -connect ' + args.ipAdd + ':' + args.whichPort + ' 2>&1'
     # Send the command string to subprocess for execution
-    sslout = subprocess.check_output(cmdstr, shell=True)
-    if ":error:" in sslout:
+    try:
+        sslout = subprocess.check_output(cmdstr, shell=True, stderr=subprocess.STDOUT)
+    except:
+        sslout = b':error:'
+
+    if ":error:" in sslout.decode():
       print(bcolors.FAIL + '[-] ' + item + ' NOT supported!' + bcolors.ENDC)
     else:
       print(bcolors.OKBLUE + '[+] ' + item + ' supported!!!' + bcolors.ENDC)
       
 if __name__ == '__main__':
   main()
+
